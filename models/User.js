b816cacdee381fb362b00cf5961584708cb209ba
erockenmeyer <erikrockenmeyer@gmail.com>
2022-06-03 15:30:50 -0500

    created user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model, SchemaTypes } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^.+@.+\..+$/
+        },
+        // show a user's thoughts
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        // also show their friends
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+// count their friend list
+UserSchema.virtual('friendCoutn').get(function () {
+    return this.friends.length;
+})
